55ea218ed1294d43911f7bb09ea21105909d20b5
Lucy Sanders <lucyhsanders@gmail.com>
2019-10-10 16:30:55 -0600

    moved REQ in back from peer

--- src/components/MainPage/BackFromPeer.js
+++ src/components/MainPage/BackFromPeer.js
@@ -21,6 +21,13 @@ function BackFromPeer(props) {
   const { classes, options } = props;
   return (
     <div>
+    <RadioInput id="deter" options={["approve", "deny"]} label="Determination" updateValue={props.handleInputs} values={props.values} />
+      {props.values.deter === "deny" 
+        ? <DeniedInputs denialId="rationale" handleInputs={props.handleInputs} values={props.values} />
+        : props.values.deter === "send to medical director"
+          ? <CriteriaInputs handleInputs={props.handleInputs} values={props.values} /> 
+          : <Checkbox id="allMet" value={props.values.allMet} updateValue={props.handleInputs} label="All crtieria met" disabled={props.disableAllMet} />
+      }
       <RadioInput id="lob" options={["commercial", "FEP", "GP"]} label="LOB" updateValue={props.handleInputs} values={props.values} />      
       { props.values.lob !== "FEP" 
         ? <RadioInput id="claimType" options={options.claimTypeOptions} label="Claim Type" updateValue={props.handleInputs} values={props.values} />
@@ -30,15 +37,12 @@ function BackFromPeer(props) {
         : null}                         
       <RadioInput id="claimSystem" options={options.claimSystemOptions} label="Claim System" updateValue={props.handleInputs} values={props.values} />             
       <ReactSelect id="pend" suggestions={suggestions(setPendOrder(options.pendOptions, props.values.lob))} label="Suspension" updateValue={props.handlePendInput} values={props.values} value={props.values.pend} />             
-      <TextInput id="req" placeholder="Enter number" label="REQ-" onBlur={props.handleInputs} values={props.values} />
-      <TextInput id="code" placeholder="" label="Suspended Codes" onBlur={props.handleInputs} values={props.values} />
-      <RadioInput id="deter" options={["approve", "deny"]} label="Determination" updateValue={props.handleInputs} values={props.values} />
-      {props.values.deter === "deny" 
-        ? <DeniedInputs denialId="rationale" handleInputs={props.handleInputs} values={props.values} />
-        : props.values.deter === "send to medical director"
-          ? <CriteriaInputs handleInputs={props.handleInputs} values={props.values} /> 
-          : <Checkbox id="allMet" value={props.values.allMet} updateValue={props.handleInputs} label="All crtieria met" disabled={props.disableAllMet} />
+      {props.values.deter === "approve"
+        ? <TextInput id="req" placeholder="Enter number" label="REQ-" onBlur={props.handleInputs} values={props.values} />
+        : null
       }
+      <TextInput id="code" placeholder="" label="Suspended Codes" onBlur={props.handleInputs} values={props.values} />
+
     <Divider variant="fullWidth" />
     <div className={classes.notes}>
       <BackFromPeerNotes values={props.values} />
