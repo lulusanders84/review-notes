8cedecf8948fc098d2a8fc5590250ba6fe89e172
Lucy Sanders <lucyhsanders@gmail.com>
2019-10-25 20:44:06 -0600

    amended add claim button

--- src/components/ClaimCount/AddClaimButton.js
+++ src/components/ClaimCount/AddClaimButton.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import * as claimCountutils from '../../utils/ClaimCount';
 import { updateClaimLog } from '../../actions';
 import { connect } from 'react-redux';
+import { handleInputs } from '../../actions';
 
 const { formatClaim } = claimCountutils;
 const useStyles = makeStyles({
@@ -14,9 +15,13 @@ const useStyles = makeStyles({
 function AddClaimButton(props) {
   const classes = useStyles();
   const {values} = props;
+  const [label, setLabel] = React.useState("Add Claim to Log");
   const handleClick = () => {
     const newClaim = formatClaim(values);
     props.dispatch(updateClaimLog(newClaim));
+    setLabel("Added");
+    props.dispatch(handleInputs({name: "req", value: ""}))
+    setTimeout(function(){ setLabel("Add Claim to Log"); }, 2000)
   }
   const disabled = props.values.req === "" ? true : false;
   return (
@@ -25,9 +30,10 @@ function AddClaimButton(props) {
       onClick={e => {handleClick()}}
       disabled={disabled}
     >
-      Add Claim to Log
+      {label}
     </Button>
   )
 }
+
 
 export default connect()(AddClaimButton)
