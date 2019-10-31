a101d0e8d313633cbd8f50a2bc5207820497e914
Lucy Sanders <lucyhsanders@gmail.com>
2019-10-31 11:43:15 -0600

    fixed daysleft = 0

--- src/utils/ClaimCount/getDailyTarget.js
+++ src/utils/ClaimCount/getDailyTarget.js
@@ -3,13 +3,16 @@ export const getDailyTarget = (claimLog, claimGoal, workdays) => {
   const todayUnix = date.getTime();
   const month = date.getMonth();
   const days = workdays[month].length;
-  const daysLeft = workdays[month].reduce((acc, day) => {
+  let daysLeft = workdays[month].reduce((acc, day) => {
     const dayUnix = new Date(day).getTime();
     if(dayUnix > todayUnix) {
       acc.push(day)
     }
     return acc;
   }, []).length;
+  if(daysLeft === 0) {
+    daysLeft = 1
+  };
   const firstOfTheMonth = new Date(`${month + 1}/1/${date.getFullYear()}`).getTime();
   const claimsSoFar = claimLog.reduce((acc, claim) => {
     if(claim.dateTime > firstOfTheMonth) {
