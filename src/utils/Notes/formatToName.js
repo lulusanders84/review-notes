874e825d8a89bf05190c03a29caf6b10e4d53787
Lucy Sanders <lucyhsanders@gmail.com>
2019-11-05 11:39:39 -0700

    added 'or' to non cap words

--- src/utils/Notes/formatToName.js
+++ src/utils/Notes/formatToName.js
@@ -1,10 +1,11 @@
 import {capWord } from './capitalizeWord';
 import {prepositions} from './prepositions';
+import { nonCapWords } from './nonCapWords';
 
 export const formatToName = (entry) => {
     const nameArray = entry.trim().split(" ");
     return nameArray.map(word => {
-        return prepositions.includes(word) 
+        return [...prepositions, ...nonCapWords].includes(word) 
             ? word
             : capWord(word); 
     }).join(" ");
