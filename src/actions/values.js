9c1e2747afa753792c10fca0666e62b9bf31bfe3
Lucy Sanders <lucyhsanders@gmail.com>
2019-11-14 11:28:55 -0700

    fixed provider bug

--- src/actions/values.js
+++ src/actions/values.js
@@ -6,11 +6,14 @@ const savePends = savingPendsUtils.savePends;
 export const handleInputs = value => (dispatch, getState) => {
   if(value.name === "benefits" || value.name === "fepBenefits") {
     value = handleBenefits(value);
-  }
+  };
   if(value.name === "policy") {
     const policiesSet = new Set(getPolicies(value.value));
     value.value = Array.from(policiesSet)
-  }
+  };
+  if(value.value === null) {
+    value.value = ""
+  };
   dispatch(setValue(value));
   const values = getState().values;
   const newValues = handleInputsChange(value, values);
