9db841ccd9b32b1a502ebe7cb2903c0fbae3245e
Lucy Sanders <lucyhsanders@gmail.com>
2019-11-20 11:35:10 -0700

    added search

--- src/classes/Values/Search.js
+++ src/classes/Values/Search.js
@@ -1,60 +1,23 @@
 import { Relationship } from "./Relationship";
 
 export class Search extends Relationship {
-  constructor(keyValue) {
+  constructor(keyValue, ranges) {
     super(keyValue);
+    this.ranges = ranges;
     this.setRelationship(this.relationship)
   }
-  relationship = (input, keyValue, range) => {
-    const { name, value } = input;
-    keyValue = capWord(keyValue);
-    const storageLocation = `${name.toLowerCase()}${keyValue}Pairs`;
-    return getValueFromPair(storageLocation, value);      
-  }
-}
-
-export const setPolicyByCode = (codes, lob) => {
-  const isJ3490 = codes.includes("J3490");
-  const policies = codes && !isJ3490
-    ? lob === "FEP" 
-      ? fep(codes)
-      : bcbsmn(codes)
-    : [];
-  return setValueAndLabel(policies);
-}
-
-const bcbsmn = (codes) => {
-  let policies = [];
-  codes.forEach(code => {
-    policies = bcbsmnCodes[code] ? [...policies, ...bcbsmnCodes[code]] : policies;
-  })
-  return policies;
-}
-const fep = (codes) => {
-  const policies =  window.localStorage.getItem("fepPolicies") ? JSON.parse(window.localStorage.getItem("fepPolicies")) : fepPoliciesOnFile;
-  return policies.reduce((acc, policy) => {
-    const hcpcs = policy["HCPCS"] === "No HCPCS" || policy["HCPCS"] === ""
-      ? []
-      : policy["HCPCS"].split(",");
-    const cpts = policy["CPT"] === "No CPT" || policy["CPT"] === ""
-      ? []
-      : policy["CPT"].split(",");
-    const codesList = [...hcpcs, ...cpts].map(code => { return code.toUpperCase().trim()});
-    codes.forEach(code => {
-      if(code !== ""){
-        if(codesList.includes(code)) {
-          acc.push(policy)
-        }
-      }
+  relationship = (input) => {
+    const { value } = input;
+    let results = [];
+    this.ranges.forEach(range => {
+      const rangeResults = range.reduce((acc, rangeItem) => {
+        rangeItem === value
+          ? acc.push(rangeItem)
+          : null;
+        return acc;
+      }, [])
+    results = [...results, ...rangeResults];
     })
-    return acc;
-  }, []) 
-}
-
-const setValueAndLabel = (policies) => {
-  policies.forEach(policy => {
-    policy.value = policy["Policy #"];
-    policy.label = `${policy["Policy #"]}: ${policy["Full Policy"]}`
-  })
-  return policies;
+    return results;    
+  }
 }
