23e9cc917f2063611b85d588caaa57417ddc3446
Lucy Sanders <lucyhsanders@gmail.com>
2019-12-16 18:15:35 -0700

    fixed sort error

--- src/utils/AutoComplete/sortSuggestionsAlphabetically.js
+++ src/utils/AutoComplete/sortSuggestionsAlphabetically.js
@@ -1,9 +1,11 @@
 export const sortSuggestionsAlphabetically = (options) => {
-  return options.sort((a, b) => {
+  if(options) {
+    return options.sort((a, b) => {
     if(a.value  < b.value) {
       return -1;
     } else if (a.value > b.value) {
       return 1;
     } else return 0
   })
+  } else return [];
 }
