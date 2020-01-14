fc8b0fff07cafbd64218848b31814d5f764c2346
Lucy Sanders <lucyhsanders@gmail.com>
2020-01-13 21:22:56 -0700

    fixed formatting issues

--- src/utils/Notes/formatToName.js
+++ src/utils/Notes/formatToName.js
@@ -1,12 +1,15 @@
 import {capWord } from './capitalizeWord';
 import {prepositions} from './prepositions';
 import { nonCapWords } from './nonCapWords';
+import { allCapWords } from './allCapWords';
 
 export const formatToName = (entry) => {
     const nameArray = entry.trim().split(" ");
     return nameArray.map(word => {
         return [...prepositions, ...nonCapWords].includes(word) 
             ? word
-            : capWord(word); 
+            : allCapWords.includes(word.toUpperCase())
+                ? word.toUpperCase()
+                : capWord(word); 
     }).join(" ");
 }
