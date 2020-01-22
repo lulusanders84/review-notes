c51e3507430c4f3ebe5d2de98d9907b2c8f7d84a
Lucy Sanders <lucyhsanders@gmail.com>
2020-01-21 21:30:24 -0700

    added routing for medicare local and home

--- src/data/queues.js
+++ src/data/queues.js
@@ -1,21 +1,30 @@
 export const queues = {
-    "local": "9665GENADJ",
-    "home": "9640MRADJ",
-    "host": {
-        "adjustment": "9645BCHADJ",
-        "live": "9645HOSUML"
-    },
-    "hormel": {
-        "adjustment": "9665HORADJ",
-        "live": "9641HORITS"
-    },
-    "employee": {
-        "local": "9665MNEMP",
-        "home": "9641EMPGRP"
+    "commercial": {
+        "employee": {
+            "local": "9665MNEMP",
+            "home": "9641EMPGRP"
+        },
+        "foreign": "9640FORCLM",
+        "home": "9640MRADJ",  
+        "hormel": {
+            "adjustment": "9665HORADJ",
+            "live": "9641HORITS"
+        },
+        "host": {
+            "adjustment": "9645BCHADJ",
+            "live": "9645HOSUML"
+        },
+        "local": "9665GENADJ",        
     },
     "fep": "9712FEPADJ",
-    "foreign": "9640FORCLM",
-    "platinum blue": "9655PBADJ",
-    "med supp": "9656MSADJ",
-    "mapd": "9657MAADJ"
+    "gp": {
+        "mapd": {
+            "local": "9657MAADJ",
+            "home": "9657MAITS"
+        },  
+        "med supp": "9656MSADJ",    
+        "platinum blue": "9655PBADJ",            
+    },
+    
+    
 }
