ff3ca6b29bade8a2d76bdd0cc091cf5fa3895759
Lucy Sanders <lucyhsanders@gmail.com>
2020-08-04 20:02:16 -0600

    updating suggestions with console.log

--- src/redux/actions/suggestions.js
+++ src/redux/actions/suggestions.js
@@ -10,6 +10,7 @@ export const setNewOptions = (lob) => (dispatch) => {
 }
 
 const pendOptions = (lob)=> {
+  console.log(pends, fepPends);
   return lob === "FEP" ? [...fepPends, ...pends] : pends;
 }
 const pendSuggestions = (lob) => {
