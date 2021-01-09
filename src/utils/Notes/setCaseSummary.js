b6eefb2b9eb3983bfa2a00caf19221ead4e6e2ab
Lucy Sanders <lucyhsanders@gmail.com>
2021-01-09 12:04:59 -0700

    updated case summary

--- src/utils/Notes/setCaseSummary.js
+++ src/utils/Notes/setCaseSummary.js
@@ -18,7 +18,7 @@ export const setCaseSummary = (values) => {
     const modifier22 = values.pend && values.pend.some(pend => {return pend.value === "R5027"})
         ? values.deter === "approve" 
             ? " Operative report demonstrates increased complexity warranting additional allowance."
-            : " Operative report doesn't demonstate increased complexity or unusual services that would warrant any additional allowance."
+            : " Operative report doesn't demonstrate increased complexity or unusual services that would warrant any additional allowance."
         : ""; 
     const checkServiceFirstChar = vowels.includes(service.charAt(0).toLowerCase());
     serviceVerb = serviceType === "DME" && checkServiceFirstChar ? serviceVerb + "n" : serviceVerb;
