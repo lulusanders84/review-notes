4307141f2a0b27ed87cbd7ede00b2e701128f66b
Lucy Sanders <lucyhsanders@gmail.com>
2021-01-09 11:53:51 -0700

    add policy type in MCR note

--- src/utils/Notes/setPolicyString.js
+++ src/utils/Notes/setPolicyString.js
@@ -2,13 +2,21 @@ export const setPolicyString = (values, field) => {
   return values.policy.length > 0 
     ? values.policy.map(policy => {
       const number = policy["Policy #"];
+      const policyType = setPolicyType(number);
       const name = number === "InterQual"
         ? setInterqual(values, field)
-        : number;
+        : `${policyType} ${number}`;
       return name}).join(" / ") 
     : "N/A";
 }
 
+const setPolicyType = (number) => {
+  return number !== "InterQual"
+    ? isNaN(number.charAt(0))
+      ? "BCBSMN"
+      : "FEP"
+    : ""
+}
 export const setInterqual = (values, field) => {
   const { iqYear, iqLoc, iqSubset } = values;
   switch(field) {
