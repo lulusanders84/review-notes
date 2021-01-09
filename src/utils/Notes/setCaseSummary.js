330099a58e6ced5596e8ebd78120eb3b2958b828
Lucy Sanders <lucyhsanders@gmail.com>
2021-01-09 11:43:02 -0700

    updated case summary for R5027

--- src/utils/Notes/setCaseSummary.js
+++ src/utils/Notes/setCaseSummary.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { vowels } from "./vowels";
 import { formatToName } from "./formatToName"; 
 import { setPricingNote } from "./setPricingNote";
 import { formatToSentence } from './formatToSentence';
-// import { serviceTypes } from '../../data/serviceTypes';
 import { CapWords } from '../../classes/CapWords'
 
 export const setCaseSummary = (values) => {
@@ -11,19 +9,12 @@ export const setCaseSummary = (values) => {
     const service = formatValue(values.service);
     const provider = values.provider !== undefined ? formatToName(values.provider.toLowerCase()) : "";
     const diagnosis = formatValue(values.diagnosis);
-    // const type = serviceTypes.find(type => type["Service Type"] === serviceType);
-    // let serviceVerb = type["Verb"] ? type["Verb"] : "underwent";
     const pricingSummary = setPricingNote(values, "summary");
     const modifier22 = values.pend && values.pend.some(pend => {return pend.value === "R5027"})
         ? values.deter === "approve" 
             ? " Operative report demonstrates increased complexity warranting additional allowance."
-            : " Operative report doesn't demonstate increased complexity or unusual services that would warrant any additional allowance."
+            : " Operative report doesn't demonstrate increased complexity or unusual services that would warrant any additional allowance."
         : ""; 
-    // const checkServiceFirstChar = vowels.includes(service.charAt(0).toLowerCase());
-    // serviceVerb = serviceType === "DME" && checkServiceFirstChar ? serviceVerb + "n" : serviceVerb;
-    // let proTypeVerb = type["ProVerb"] 
-    //     ? type["ProVerb"]
-    //     :values.proType === "professional" ? "by" : "at";
     const summary = formatToSentence(values.summary);
     const dose = values.drugReview && values.lob === "commercial"
         ? `(${values.dose} ${values.doseUnit})`
