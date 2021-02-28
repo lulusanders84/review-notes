b61dbd8f47d94d60eef7b5cdad1d2b84b3e2ccbc
Lucy Sanders <lucyhsanders@gmail.com>
2021-02-27 20:11:21 -0700

    added par status to decision review note

--- src/components/ReviewNotes/NoteTemplates/BackFromPeer.js
+++ src/components/ReviewNotes/NoteTemplates/BackFromPeer.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import TextInput from '../../Inputs/TextInput';
+import * as Fields from '../../Inputs/FieldInputs';
 import BackFromPeerNotes from '../../BackFromPeerNotes/BackFromPeerNotes';
 import DeterInputs from '../../Inputs/DeterInputs';
 import { Divider } from '@material-ui/core';
@@ -18,15 +19,16 @@ function BackFromPeer(props) {
   return (
     <div style={{width: "100%"}}>
       <DeterInputs  />
-      <ClaimInfoInputs  />           
-      <TextInput id="req" placeholder="Enter number" label="REQ-"  />
+      <ClaimInfoInputs  />
+      <Fields.Par />           
+      <Fields.Req />
       {props.values.denialType !== "entire claim" && props.values.deter === "deny"
         ?<TextInput id="code" placeholder="" label="Suspended Codes"  />
         : null
         }
       {props.values.deter === "approve"
         ? <div>
-        <PendInput  />  
+        <PendInput />  
           </div>
         : null
       }
