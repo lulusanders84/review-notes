46a620ddaf111c9853b09613f9487ac3b396e2aa
Lucy Sanders <lucyhsanders@gmail.com>
2021-03-16 11:32:09 -0600

    updated capWordButton functions to a class

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import styles from '../../styles/noteStyles';
-import { CapWordButtons } from '../CapWordButtons/CapWordButtons';
+import CapWordButtons from '../CapWordButtons/CapWordButtons';
 
 const useStyles = makeStyles(() => (styles));
 
