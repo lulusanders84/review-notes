580b93d6a0448e761ab46d8bcb70783dde9c1c80
Lucy Sanders <lucyhsanders@gmail.com>
2021-04-04 19:08:21 -0600

    added clean array to make sure add on summary doesn't have duplicates

--- src/utils/Values/editAddOnSummary.js
+++ src/utils/Values/editAddOnSummary.js
@@ -1,8 +1,10 @@
 import { addOnSummaryContent } from "../../data/addOnSummaryContent"
+import { cleanArray } from '../'
 
 export const editAddOnSummary = (addOnSummary, addOnKey, isMet) => {
   const addOnText = addOnSummaryContent[addOnKey]
-  return isMet
+  const newAddOnSummary = isMet
     ? [...addOnSummary, addOnText]
     : addOnSummary.filter(item => item !== addOnText)
+  return cleanArray(newAddOnSummary)
 }
